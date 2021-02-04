6a11b0db3af2b864004d261651b5c533e6156e5e
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2021-02-04 18:24:24 -0300

    Retirado o arredondamento

--- Exercícios/Exercícios008.py
+++ Exercícios/Exercícios008.py
@@ -4,7 +4,5 @@
 
 metros = float(input('Digite um valor para ser convertido: ')) #O valor digitado será guardado na variável metros. Vale ressaltar que a variável metros aceita valores float.
 
-print('{:.0f} metros é igual a {:.2f} centímetros'.format(metros, metros*100)) #O valor em metros será multiplicado por 100, assim obteremos o valor convertido em cm.
-print('{:.0f} metros é igual a {:.2f} milímetros'.format(metros, metros*1000)) #O valor em metros será multiplicado por 1000, assim obteremos o valor convertido em mm.
-
-#Obs: Os valores obtidos em centímetros e milímetros serão mostrados com 2 casas decimais, ou seja, com 2 números após a vírgula.
+print('{} metros é igual a {} centímetros'.format(metros, metros*100)) #O valor em metros será multiplicado por 100, assim obteremos o valor convertido em cm.
+print('{} metros é igual a {} milímetros'.format(metros, metros*1000)) #O valor em metros será multiplicado por 1000, assim obteremos o valor convertido em mm.
