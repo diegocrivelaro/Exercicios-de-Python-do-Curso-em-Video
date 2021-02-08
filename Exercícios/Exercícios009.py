f1417fe4f17455e53d6fb7817b37b7a254e87807
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2021-02-08 14:22:34 -0300

    Foi criado o exercício 009

---
+++ Exercícios/Exercícios009.py
@@ -0,0 +1,18 @@
+'''
+9 - Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada. 
+'''
+
+número = int(input('Digite um número para tabuada: ')) #O número será guardado na variável número. 
+
+print('='*20) #Enfeite
+print("{} x 1 = {}".format(número, número*1)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 1.
+print("{} x 2 = {}".format(número, número*2)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 2.
+print("{} x 3 = {}".format(número, número*3)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 3.
+print("{} x 4 = {}".format(número, número*4)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 4.
+print("{} x 5 = {}".format(número, número*5)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 5.
+print("{} x 6 = {}".format(número, número*6)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 6.
+print("{} x 7 = {}".format(número, número*7)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 7.
+print("{} x 8 = {}".format(número, número*8)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 8.
+print("{} x 9 = {}".format(número, número*9)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 9.
+print("{} x 10 = {}".format(número, número*10)) #Na primeira máscara será mostrado o valor que está na variável número. Na segunda máscara o valor da variável será multiplicada pelo 10.
+print('='*20) #Enfeite 
