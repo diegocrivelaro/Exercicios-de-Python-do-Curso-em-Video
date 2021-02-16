63f7f3bd6e3ff2a878dfeaa44195afd4b63a94a7
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2021-02-16 13:26:18 -0300

    Mudança pequena

--- Exercícios/Exercício008.py
+++ Exercícios/Exercício008.py
@@ -4,5 +4,7 @@
 
 metros = float(input('Digite um valor para ser convertido: ')) #O valor digitado será guardado na variável metros. Vale ressaltar que a variável metros aceita valores float.
 
-print('{} metros é igual a {} centímetros'.format(metros, metros*100)) #O valor em metros será multiplicado por 100, assim obteremos o valor convertido em cm.
-print('{} metros é igual a {} milímetros'.format(metros, metros*1000)) #O valor em metros será multiplicado por 1000, assim obteremos o valor convertido em mm.
+print('='*20) #Será criado 20 "="
+print('» {} metros é igual a {} centímetros'.format(metros, metros*100)) #O valor em metros será multiplicado por 100, assim obteremos o valor convertido em cm.
+print('» {} metros é igual a {} milímetros'.format(metros, metros*1000)) #O valor em metros será multiplicado por 1000, assim obteremos o valor convertido em mm.
+print('='*20) #Será criado 20 "="
