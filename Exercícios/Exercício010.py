eef974ebb42f4e30cc9d7ad8835d013b66a81db9
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2021-02-16 13:27:36 -0300

    Adicionamento do "R$" na mensagem

--- Exercícios/Exercício010.py
+++ Exercícios/Exercício010.py
@@ -2,8 +2,8 @@
 10 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 '''
 
-carteira = float(input('Digite o valor disponível na sua carteira: ')) #O valor será guardado na variável carteira.
-DolarHoje = 5.31 #O valor do dólar está sendo guardado aqui.
+carteira = float(input('Digite o valor disponível na sua carteira: R$')) #O valor será guardado na variável carteira.
+DolarHoje = 5.37 #O valor do dólar está sendo guardado aqui.
 
 print('='*30) #Enfeite 
 print('Cotação do Dólar: R${}\nVocê pode comprar apenas {:.2f} dólares.'.format(DolarHoje, carteira/DolarHoje)) #O algoritmo irá mostrar o valor do dólar, e depois irá pegar o valor da carteira e dividir pelo valor do dolar.
