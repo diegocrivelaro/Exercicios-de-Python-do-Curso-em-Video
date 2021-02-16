2abfac31e0e363d469b92d8892ff52b488931035
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2021-02-16 13:26:31 -0300

    Mudança pequena

--- Exercícios/Exercício008b.py
+++ Exercícios/Exercício008b.py
@@ -4,11 +4,11 @@
 
 metros = float(input('Digite um valor para ser convertido: ')) #O valor digitado será guardado na variável metros. Vale ressaltar que a variável metros aceita valores float.
 
-print('='*25)
+print('='*20) #Será criado 20 "="
 print('{}m → {}km'.format(metros, metros/1000)) #O valor em metros será dividido por 100, assim obteremos o valor convertido em km.
 print('{}m → {}hm'.format(metros, metros/100)) #O valor em metros será dividido por 100, assim obteremos o valor convertido em hm.
 print('{}m → {}dam'.format(metros, metros/10)) #O valor em metros será dividido por 10, assim obteremos o valor convertido em dam.
 print('{}m → {}dm'.format(metros, metros*10)) #O valor em metros será multiplicado por 10, assim obteremos o valor convertido em dm.
 print('{}m → {}cm'.format(metros, metros*100)) #O valor em metros será multiplicado por 100, assim obteremos o valor convertido em cm.
 print('{}m → {}mm'.format(metros, metros*1000)) #O valor em metros será multiplicado por 1000, assim obteremos o valor convertido em mm.
-print('='*25)
+print('='*20) #Será criado 20 "="
