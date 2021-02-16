3aed644ca6bad5372cf0b97c3d2386cdd5327d17
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2021-02-16 13:48:15 -0300

    Melhoramento na documentação

--- Exercícios/Exercício012.py
+++ Exercícios/Exercício012.py
@@ -6,9 +6,10 @@
 
 preço_produto = float(input("Preço do produto: ")) #O preço do produto será armazenado na variável preço_produto. Lembrando que o valor pode ser aceito em decimal!
 desconto = int(input("Desconto: ")) #O desconto será armazenado na variável desconto.
-valor_do_desconto = preço_produto*(desconto/100) #O resultado será guardado na variável valor_do_desconto.
-preço_com_desconto = preço_produto - valor_do_desconto #O resultado será guardado na variável preço_com_desconto.
+
+valor_do_desconto = preço_produto*(desconto/100) #Parte matemática.
+preço_com_desconto = preço_produto - valor_do_desconto #Parte matemática.
 
 print("="*30) #Enfeite
-print("Preço do Produto: {:.2f}\nDesconto de: {}%\nValor do desconto: {:.2f}\nPreço com desconto aplicado: {:.2f}".format(preço_produto, desconto, valor_do_desconto, preço_com_desconto)) #Mostrando na tela
+print("Preço do Produto: R${:.2f}\nDesconto de: {}%\nValor do desconto: R${:.2f}\nPreço com desconto aplicado: R${:.2f}".format(preço_produto, desconto, valor_do_desconto, preço_com_desconto)) #Mostrando na tela
 print("="*30) #Enfeite
--- Exercícios/Exercício013.py
+++ Exercícios/Exercício013.py
@@ -2,14 +2,16 @@
 13 - Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 """
 
-salário_atual = float(input("Salário atual: ")) #O salário é guardado na variável salário_atual.
+#Obs: Resolvi deixar o usuário escolher o reajuste.
+
+salário_atual = float(input("Salário atual: R$")) #O salário é guardado na variável salário_atual.
 porcentagem_do_aumento = int(input("Porcentagem do aumento: ")) #A porcentagem é guardada na variável porcentagem_do_aumento.
 
 valor_do_aumento = salário_atual*(porcentagem_do_aumento/100) #Parte matemática.
 salário_com_aumento = salário_atual + valor_do_aumento #Parte matemática.
 
 print("="*30) #Enfeite
-print("O valor do aumento é de: R%{:.2f}".format(valor_do_aumento))
+print("O valor do aumento é de: R${:.2f}".format(valor_do_aumento))
 print("Salário reajustado: R${:.2f}".format(salário_com_aumento))
 print("="*30) #Enfeite
 
